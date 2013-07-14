7b22372e13fb8defe1c27a839a7bce13a752810a
giawa <giawa@hotmail.com>
2013-07-13 18:21:31 -0700

    Commit of the code, LICENSE and README for OpenGL4Mac, which is based on the OpenGL library in github.com/giawa/opengl4mac

---
+++ OpenGLMac/Program.cs
@@ -0,0 +1,78 @@
+using System;
+
+using Tao.FreeGlut;
+using OpenGL;
+
+namespace OpenGLTest
+{
+	public class Program
+	{
+		private static int width = 1280, height = 720;
+		private static VAO cube;
+
+		public static void Main()
+		{
+			Glut.glutInit ();
+			Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+			Glut.glutInitWindowSize(width, height);
+			Glut.glutCreateWindow("OpenGL");
+			Glut.glutIdleFunc (IdleFunc);
+			Glut.glutDisplayFunc (DisplayFunc);	// this is required on the Mac implementation of GLUT
+
+			// enable wireframe mode so that we can see the faces of the cube
+			Gl.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+
+			// create the shader program
+			ShaderProgram program = new ShaderProgram(vertexShaderSource, fragmentShaderSource);
+
+			// set the color to blue
+			program["color"].SetValue(new Vector3(0, 0, 1));
+
+			// set up some defaults for the shader program project and modelview matrices
+			program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+			program["modelview_matrix"].SetValue(Matrix4.CreateTranslation(new Vector3(2, 2, -10)) * Matrix4.CreateRotation(new Vector3(1, -1, 0), 0.2f));
+
+			// create a cube
+			cube = OpenGL.Constructs.Geometry.CreateCube(program, new Vector3(-1, -1, -1), new Vector3(1, 1, 1));
+
+			Glut.glutMainLoop ();
+		}
+
+		private static void IdleFunc()
+		{
+			// clear the screen (both color and depth)
+			Gl.Viewport(0, 0, width, height);
+			Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+			// enable the shader program for our cube
+			cube.Program.Use();
+			cube.Draw();
+
+			Glut.glutSwapBuffers();
+		}
+
+		private static void DisplayFunc()
+		{
+		}
+
+		public static string fragmentShaderSource = @"
+uniform vec3 color;
+ 
+void main(void)
+{
+  gl_FragColor = vec4(color, 1);
+}";
+
+		public static string vertexShaderSource = @"
+uniform mat4 projection_matrix;
+uniform mat4 modelview_matrix;
+ 
+attribute vec3 in_position;
+ 
+void main(void)
+{
+  gl_Position = projection_matrix * modelview_matrix * vec4(in_position, 1);
+}";
+	}
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-opengl4mac
-==========
+# OpenGL For Mac
+This is some sample code to get my OpenGL library up and running on Mac OS X.  This project will open in Xamarin Studio with the Mono Framework installed.  It should also work in Mono Develop and Visual Studio, but both of those are untested.
 
-A basic Mac OS X project that will create a window using GLUT and then draw a cube in OpenGL.
+## License
+Check the included [LICENSE.md](https://github.com/giawa/opengl4mac/blob/master/LICENSE.md) file for the license associated with this code.
+
+## Building the Project
+This project includes a .sln and .csproj file which will initialize a window using GLUT and then will draw a cube in wireframe mode.  You can check out the write-up for this project at [http://giawa.com](http://giawa.com)
